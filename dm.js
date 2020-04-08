f9b3004e20194a1f96838d21a75bfbb1ea206785
Ethan Agranoff <37985812+ethana1234@users.noreply.github.com>
2020-04-07 22:05:57 -0600

    Update dm.js

--- dm.js
+++ dm.js
@@ -1,7 +1,7 @@
-var CONSUMER_KEY = 'Dt6AFWtQHc9BaCgrEQDTe5YwN',
-    CONSUMER_SECRET = 'NTvkWVOf8kZlf6RR3VMSPsomaoQYybbgIAWg8kh5XDFuMMPppx',
-    ACCESS_TOKEN = '1052252249093623808-f82Ee9WYzC8SFOeGfdFMmUEY2qwvHK',
-    ACCESS_TOKEN_SECRET = 'ZaSF4J0NnesGjLjZxuD915YRg2MChW2SiImumujwlHKmj',
+var CONSUMER_KEY = '',
+    CONSUMER_SECRET = '',
+    ACCESS_TOKEN = '',
+    ACCESS_TOKEN_SECRET = '',
     Twit = require('twit'),
     config = {
     /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */
